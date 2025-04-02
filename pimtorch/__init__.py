9de1a802a77255bd9d71eaaa5df33b8515a80da3
zhou <1499403578@qq.com>
2025-04-02 13:38:15 +0800

    stable version

--- pimtorch/__init__.py
+++ pimtorch/__init__.py
@@ -2,7 +2,7 @@
 # @File  : __init__.py
 # @Author: Zhou
 # @Date  : 2024/12/27
-__all__ = ['SlicedData', 'DPETensor', 'RE', 'ABSE', 'SNR', 'dot_high_dim']
+__all__ = ['SlicedData', 'DPETensor', 'RE', 'ABSE']
 __version__ = '0.0.4'
 
 from .data_formats import *
